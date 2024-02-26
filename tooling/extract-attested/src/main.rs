9dcf9f57f5e495b4c2b95971c922852526835b84
Ryan1729 <Ryan1729@gmail.com>
2024-02-26 00:06:00 -0700

    add cli args

--- tooling/extract-attested/src/main.rs
+++ tooling/extract-attested/src/main.rs
@@ -1,13 +1,14 @@
 use std::collections::{BTreeSet, HashSet};
 use serde::{Serialize, Deserialize};
-use serde_json::Value;
 use std::io::Write;
 
 type Res<A> = Result<A, Box<dyn std::error::Error>>;
 
+// Let's keep the attested values in a consistent order.
+type AttestedSet = BTreeSet<(String, String)>;
+
 struct Words {
-    // Let's keep the attested values in a consistent order.
-    attested: BTreeSet<(String, String)>,
+    attested: AttestedSet,
     adjectives: Vec<String>,
     nouns: Vec<String>,
 }
@@ -42,6 +43,41 @@ impl From<WordsVecs> for Words {
 fn main() -> Res<()> {
     const PATH: &str = "../../words.js";
 
+    enum Mode {
+        WriteBack,
+        PrintFound,
+        PrintAdditional,
+    }
+
+    let mode = {
+        let mut mode = Mode::WriteBack;
+
+        let mut seen_exe_name = false;
+        for arg in std::env::args() {
+            match arg.as_str() {
+                "--write-back" => {
+                    mode = Mode::WriteBack;
+                }
+                "--print-found" => {
+                    mode = Mode::PrintFound;
+                }
+                "--print-additional" => {
+                    mode = Mode::PrintAdditional;
+                }
+                other => {
+                    if other.trim() != "" {
+                        if seen_exe_name {
+                            return Err(format!("Unknown arg: \"{arg}\"").into());
+                        }
+                        seen_exe_name = true;
+                    }
+                }
+            }
+        }
+
+        mode
+    };
+
     let js_string = std::fs::read_to_string(PATH)?;
 
     const PREFIX: &str = "words = ";
@@ -54,93 +90,130 @@ fn main() -> Res<()> {
     let adjectives: HashSet<String> = HashSet::from_iter(words.adjectives.clone());
     let nouns: HashSet<String> = HashSet::from_iter(words.nouns.clone());
 
-    let pre_count = words.attested.len();
+    fn add_pairs(
+        attested: &mut AttestedSet,
+        // TODO Are we missing pairs that are split across lines?
+        lines: impl Iterator<Item = String>,
+        adjectives: &HashSet<String>,
+        nouns: &HashSet<String>,
+    ) -> Res<()> {
+        let pattern = |c: char|
+            c.is_whitespace()
+            || c == '.'
+            || c == '!'
+            || c == '?'
+            || c == '"'
+            || c == ':'
+            || c == ';'
+            ;
 
-    let pattern = |c: char|
-        c.is_whitespace()
-        || c == '.'
-        || c == '!'
-        || c == '?'
-        || c == '"'
-        || c == ':'
-        || c == ';'
-        ;
+        let mut adj_lower = String::with_capacity(32);
+        let mut noun_lower = String::with_capacity(32);
 
-    let mut adj_lower = String::with_capacity(32);
-    let mut noun_lower = String::with_capacity(32);
+        for line in lines {
+            let mut iter = line.split(pattern).peekable();
+            while let Some(poss_adj) = iter.next() {
+                if let Some(poss_noun) = iter.peek() {
+                    let poss_noun: &str = poss_noun;
 
-    for line in std::io::stdin().lines() {
-        let line = line?;
+                    use std::fmt::Write;
+                    if (
+                        // First so adj_lower is always up to date when we
+                        // get a match
+                        {
+                            adj_lower.clear();
 
-        let mut iter = line.split(pattern).peekable();
-        while let Some(poss_adj) = iter.next() {
-            if let Some(poss_noun) = iter.peek() {
-                let poss_noun: &str = poss_noun;
+                            write!(&mut adj_lower, "{poss_adj}")?;
+                            adj_lower.make_ascii_lowercase();
 
-                use std::fmt::Write;
-                if (
-                    // First so adj_lower is always up to date when we
-                    // get a match
-                    {
-                        adj_lower.clear();
+                            adjectives.contains(&adj_lower)
+                        }
+                        || adjectives.contains(poss_adj)
+                    )
+                    && (
+                        // First so noun_lower is always up to date when we
+                        // get a match
+                        {
+                            noun_lower.clear();
 
-                        write!(&mut adj_lower, "{poss_adj}")?;
-                        adj_lower.make_ascii_lowercase();
+                            write!(&mut noun_lower, "{poss_noun}")?;
+                            noun_lower.make_ascii_lowercase();
 
-                        adjectives.contains(&adj_lower)
+                            nouns.contains(&noun_lower)
+                        }
+                        || nouns.contains(poss_noun)
+                    ) {
+                        attested.insert((
+                            adj_lower.clone(),
+                            noun_lower.clone()
+                        ));
                     }
-                    || adjectives.contains(poss_adj)
-                )
-                && (
-                    // First so noun_lower is always up to date when we
-                    // get a match
-                    {
-                        noun_lower.clear();
-
-                        write!(&mut noun_lower, "{poss_noun}")?;
-                        noun_lower.make_ascii_lowercase();
-
-                        nouns.contains(&noun_lower)
-                    }
-                    || nouns.contains(poss_noun)
-                ) {
-                    words.attested.insert((
-                        adj_lower.clone(),
-                        noun_lower.clone()
-                    ));
                 }
             }
         }
+
+        Ok(())
     }
 
-    let post_count = words.attested.len();
+    let lines = std::io::stdin().lines().filter_map(|res| res.ok());
 
-    if post_count <= pre_count {
-        println!("No new adjective-noun pairs found.");
+    match mode {
+        Mode::WriteBack => {
+            let pre_count = words.attested.len();
 
-        return Ok(());
+            add_pairs(&mut words.attested, lines, &adjectives, &nouns)?;
+
+            let post_count = words.attested.len();
+
+            if post_count <= pre_count {
+                println!("No new adjective-noun pairs found.");
+
+                return Ok(());
+            }
+
+            let words_vecs: WordsVecs = words.into();
+
+            let mut output = std::io::Cursor::new(Vec::with_capacity(1 << 12));
+            write!(&mut output, "{PREFIX}")?;
+
+            serde_json::ser::to_writer_pretty(
+                &mut output,
+                &words_vecs
+            )?;
+
+            std::fs::write(
+                PATH,
+                output.get_ref()
+            )?;
+
+            let additional = post_count - pre_count;
+            println!(
+                "Successfully wrote {additional} more adjective-noun pair{}.",
+                if additional == 1 { "" } else { "s" }
+            );
+        },
+        Mode::PrintAdditional | Mode::PrintFound => {
+            let mut attested = AttestedSet::new();
+
+            add_pairs(&mut attested, lines, &adjectives, &nouns)?;
+
+            if let Mode::PrintAdditional = mode {
+                attested.retain(|k| {
+                    !words.attested.contains(k)
+                });
+            }
+
+            if attested.is_empty() {
+                println!("No new adjective-noun pairs found.");
+
+                return Ok(());
+            }
+
+            for (adj, noun) in attested {
+                println!("{adj} {noun}");
+            }
+        },
     }
-
-    let words_vecs: WordsVecs = words.into();
-
-    let mut output = std::io::Cursor::new(Vec::with_capacity(1 << 12));
-    write!(&mut output, "{PREFIX}")?;
-
-    serde_json::ser::to_writer_pretty(
-        &mut output,
-        &words_vecs
-    )?;
-
-    std::fs::write(
-        PATH,
-        output.get_ref()
-    )?;
-
-    let additional = post_count - pre_count;
-    println!(
-        "Successfully wrote {additional} more adjective-noun pair{}.",
-        if additional == 1 { "" } else { "s" }
-    );
 
     Ok(())
 }
